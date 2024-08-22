802d8f79241990c328976edc2691c2be61dc8df1
shinchan2007 <kishorsuresh210@gmail.com>
2024-08-22 18:36:02 +0530

    simple program to find factorial of a number using python

--- factorial.py
+++ factorial.py
@@ -2,5 +2,5 @@ number = int(input("enter a number:"))
 factorial = 1
 for i in range(1,number+1):
     factorial = factorial * i 
-print("factorial of",number,"is",factorial)
+print("factorial of",number,"is",factorial,end="")
 
